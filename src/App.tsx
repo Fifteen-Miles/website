ab0d0a5ca4f0299ee78c3781c4a17a5ba3a4f638
nathanscd <nathansscd@gmail.com>
2026-07-20 17:14:28 -0300

    Index.html

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 
    useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       wheelMultiplier: 1,
